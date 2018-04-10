03690fa1be11e46e369cca72a6ee437f4d7ef4ac
Lu Lechuan <E0036072@u.nus.edu>
2018-04-10 14:44:15 +0800

    Complete features (#6)
    * add average height feature
    
    * Add max height feature
    
    * Add unevenness feature

---
+++ src/Tetris/Features/MaxHeightFeature.java
@@ -0,0 +1,20 @@
+package Tetris.Features;
+
+import Tetris.State;
+
+public class MaxHeightFeature extends Feature{
+
+    @Override
+    public double getValue(State state) {
+        double max = 0;
+
+        int[] tops = state.getTop();
+
+        for (int i = 0; i < tops.length; i++) {
+            max = Math.max(max, tops[i]);
+        }
+
+        return max;
+    }
+
+}
