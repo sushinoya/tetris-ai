2fcc75e6a58c7a74a4b350360ba93400b81bdfbc
Suyash Shekhar <suyashshekhar@gmail.com>
2018-04-11 23:48:14 +0800

    Add Constants

---
+++ src/Tetris/Constants.java
@@ -0,0 +1,11 @@
+package Tetris;
+
+public final class Constants {
+
+    public final static int NUMBER_OF_GAMES = 100;
+    public final static int NUMBER_OF_HEURISTICS = 100;
+    public final static int SUM_OF_PROBABILITIES = 100;
+    public final static int NUMBER_OF_GENERATIONS = 100;
+    public final static int NUMBER_OF_FEATURES = 4;
+
+}
