c3b572ce7292547f17981b9380aa6becec58ff3d
Suyash Shekhar <suyashshekhar@gmail.com>
2018-04-16 22:50:37 +0800

    Update constants

--- src/Tetris/Constants.java
+++ src/Tetris/Constants.java
@@ -2,8 +2,8 @@ package Tetris;
 
 public final class Constants {
 
-    public final static int NUMBER_OF_GAMES = 3;
-    public final static int NUMBER_OF_HEURISTICS = 500;
+    public final static int NUMBER_OF_GAMES = 5;
+    public final static int NUMBER_OF_HEURISTICS = 400;
     public final static int SUM_OF_PROBABILITIES = 100;
     public final static int NUMBER_OF_GENERATIONS = 100;
     public final static int NUMBER_OF_FEATURES = 5;
