0d9851fbc2d593c63b7f46260672d0d1d0c5ec29
Haozhe <phaozhe@hotmail.com>
2018-04-16 23:44:57 +0800

    Add numOfRowsCleared feature to Heuristic

--- src/Tetris/Constants.java
+++ src/Tetris/Constants.java
@@ -6,7 +6,7 @@ public final class Constants {
     public final static int NUMBER_OF_HEURISTICS = 400;
     public final static int SUM_OF_PROBABILITIES = 100;
     public final static int NUMBER_OF_GENERATIONS = 100;
-    public final static int NUMBER_OF_FEATURES = 5;
+    public final static int NUMBER_OF_FEATURES = Heuristic.features.length;
     public final static boolean DRAW_ENABLED = false;
     public final static int WAITING_TIME = 1;
     public final static boolean IS_GENETIC_RUNNING = true;
