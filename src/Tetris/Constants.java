4125d94052f03a864394c9870d9526cb3a40d022
Haozhe <phaozhe@hotmail.com>
2018-04-18 15:42:30 +0800

    Make data Serializable

--- src/Tetris/Constants.java
+++ src/Tetris/Constants.java
@@ -2,8 +2,8 @@ package Tetris;
 
 public final class Constants {
 
-    public final static int NUMBER_OF_GAMES = 3;
-    public final static int NUMBER_OF_HEURISTICS = 100;
+    public final static int NUMBER_OF_GAMES = 5;
+    public final static int NUMBER_OF_HEURISTICS = 500;
     public final static int SUM_OF_PROBABILITIES = 100;
     public final static int NUMBER_OF_GENERATIONS = 100;
     public final static double PROBABILITY_OF_MUTATION = 0.05;
@@ -16,6 +16,7 @@ public final class Constants {
     public final static boolean USE_ZERO_INITIAL_POPULATON = false;
     public final static int WAITING_TIME = 1;
     public final static boolean IS_GENETIC_RUNNING = true;
+    public final static boolean READ_POPULATION_FROM_FILE = true;
     public final static String AVERAGE_LOG_FOR_4HEURISTICS = "best_4_averages.txt";
     public final static String AVERAGE_LOG_FOR_5HEURISTICS = "best_5_averages.txt";
 
